82bae327b057a79f2b98a016a91c8e4bdfb5ee6e
Shakib Omarni <shakshep@gmail.com>
2018-06-23 20:57:00 +0430

    Adding scripts and Swiper npm

--- NewsSystem.MVC/App_Start/BundleConfig.cs
+++ NewsSystem.MVC/App_Start/BundleConfig.cs
@@ -20,12 +20,14 @@ namespace NewsSystem.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-rtl.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-rtl.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Events-News/css").Include(
+                      "~/Content/Events-News/_Layout-EventsArticles.css"));
         }
     }
 }
